14cdccf2352f23d6703e062270f1a28149c473e2
Donatien Wallaert <donatien@MacBook-Air-de-Donatien.local>
2025-12-04 10:44:02 +0100

    update run

--- RNASeqanalysis/run.sh
+++ RNASeqanalysis/run.sh
@@ -1,11 +1,3 @@
-# a faire en amont : 
-# cd /home/ubuntu/data/mydatalocal
-# git clone https://github.com/Reprohackathon-RNASeq/Reprohackathon-RNASeq.git
-# cd Reprohackathon-RNASeq/RNASeqanalysis
-# chmod +x run.sh
-# ./run.sh
-#!/bin/bash
-
 sudo apt-get update
 sudo apt install python3.10-venv
 sudo apt install openjdk-17-jre -y
@@ -16,7 +8,3 @@ curl -s https://get.nextflow.io | bash
 chmod +x nextflow
 sudo mv nextflow /usr/local/bin/
 nextflow -version
-
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
